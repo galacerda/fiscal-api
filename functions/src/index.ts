17aeda187357c0f67265f2abf355acd0649d827a
galacerda <galacerda260401@gmail.com>
2022-04-26 23:00:48 -0300

    add new features

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import * as admin from "firebase-admin";
 const app = admin.initializeApp();
 const db = app.firestore();
 const collVehicles = db.collection("veiculos");
+const collDevices = db.collection("aparelhos");
 
 interface CallableResponse {
   status: string;
@@ -20,6 +21,35 @@ const verifyPlate = (plate: string): boolean => {
   }
 };
 
+export const getUserAuth = functions
+    .region("southamerica-east1")
+    .https.onCall(async (data) => {
+      const {deviceId} = data;
+      const {docs, empty} = await collDevices
+          .where("deviceId", "==", deviceId)
+          .get();
+      if (!empty) {
+        try {
+          const user = await admin.auth().getUser(docs[0].data().uid);
+          return {
+            status: "SUCCESS",
+            message: "deu certo",
+            payload: JSON.parse(
+                JSON.stringify({
+                  email: user.email,
+                  password: "123456aB",
+                })
+            ),
+          };
+        } catch (error) {
+          functions.logger.error("Erro ao buscar user");
+          throw new functions.https.HttpsError("invalid-argument", "deuerrado");
+        }
+      }
+      functions.logger.error("Erro ao buscar user");
+      throw new functions.https.HttpsError("invalid-argument", "deuerrado");
+    });
+
 export const consultPlate = functions
     .region("southamerica-east1")
     .https.onCall(async (data) => {
@@ -29,61 +59,59 @@ export const consultPlate = functions
       const verifiedPlate = verifyPlate(plate);
 
       if (!verifiedPlate) {
-        functions.logger.error("Erro ao consultar placa");
-        result = {
-          status: "ERROR",
-          message: "Placa inválida",
-          payload: JSON.parse(JSON.stringify({placa: plate})),
-        };
-        return result;
+        functions.logger.error("Erro ao consultar - Placa inválida");
+        throw new functions.https.HttpsError(
+            "invalid-argument",
+            "Placa invalida",
+            {placa: plate}
+        );
       }
 
       const {docs, empty} = await collVehicles
           .where("placa", "==", plate)
           .get();
 
-      if (empty) {
-        functions.logger.error("Erro ao consultar placa");
-        result = {
-          status: "ERROR",
-          message: "Placa não encontrada",
-          payload: JSON.parse(JSON.stringify({placaConsultada: plate})),
+      if (!empty) {
+        const {placa, fim} = docs[0].data();
+
+        const dateFim = new Date((fim._seconds - 10800) * 1000);
+        const docInfo = {
+          placaConsultada: plate,
         };
-        return result;
-      }
 
-      const {placa, fim, inicio} = docs[0].data();
-
-      const docInfo = {
-        placaConsultada: plate,
-        inicio: new Date((inicio._seconds - 10800) * 1000).toISOString(),
-        fim: new Date((fim._seconds - 10800) * 1000).toISOString(),
-      };
-
-      if (plate === placa) {
-        if (fim._seconds < Date.now() / 1000) {
-          result = {
-            status: "SUCCESS",
-            message: "MULTA ESSE SAFADO AGORA",
-            payload: JSON.parse(
-                JSON.stringify({...docInfo, regularizado: false})
-            ),
-          };
-        } else {
-          result = {
-            status: "SUCCESS",
-            message: "VOCÊ ESTÁ LIBERADO",
-            payload: JSON.parse(
-                JSON.stringify({...docInfo, regularizado: true})
-            ),
-          };
+        if (plate === placa) {
+          if (fim._seconds < Date.now() / 1000) {
+            result = {
+              status: "SUCCESS",
+              message: "Veiculo não está regularizado.",
+              payload: JSON.parse(
+                  JSON.stringify({...docInfo, regularizado: false})
+              ),
+            };
+          } else {
+            result = {
+              status: "SUCCESS",
+              message: `Veículo regularizado até: ${
+                dateFim.getHours() + ":" + dateFim.getMinutes()
+              } `,
+              payload: JSON.parse(
+                  JSON.stringify({...docInfo, regularizado: true})
+              ),
+            };
+          }
+          return result;
         }
-        return result;
       }
       result = {
         status: "SUCCESS",
-        message: "Produto inserido com sucesso.",
-        payload: JSON.parse(JSON.stringify(docInfo)),
+        message: "Veiculo não foi encontrado.",
+        payload: JSON.parse(
+            JSON.stringify({placaConsultada: plate, regularizado: false})
+        ),
       };
       return result;
     });
+
+// export const registerIrregularity = functions
+//     .region("southamerica-east1")
+//     .https.onCall(async (data, context) => {});
